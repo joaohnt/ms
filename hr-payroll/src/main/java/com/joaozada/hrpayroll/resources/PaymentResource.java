d7b5cf94bde39c62ec656c0729c5493822fce508
joaohnt <jpedrooo.cg@gmail.com>
2024-11-17 12:24:14 -0300

    Eureka implementation and configuration && Hystrix fault tolerance

--- hr-payroll/src/main/java/com/joaozada/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/joaozada/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.joaozada.hrpayroll.resources;
 
 import com.joaozada.hrpayroll.entities.Payment;
 import com.joaozada.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
-    @GetMapping(value="/{workerId}/days/{days}")
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
+    @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable int days) {
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, int days) {
+        Payment payment = new Payment ("garcia", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
