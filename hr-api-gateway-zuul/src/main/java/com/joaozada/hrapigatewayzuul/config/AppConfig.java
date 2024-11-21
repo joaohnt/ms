d9c289db680c1519abb5ada459e336c26fa6cab1
joaohnt <jpedrooo.cg@gmail.com>
2024-11-21 06:11:19 -0300

    zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/joaozada/hrapigatewayzuul/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.joaozada.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("MY-SECRET-KEY");
+        return converter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+}
